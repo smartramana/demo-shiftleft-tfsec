f6e2a4036eb351532bea8ee29fb4ee108fc8fdb9
smartramana <smartramana@gmail.com>
2023-04-19 14:00:58 -0400

    Update main.tf

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -23,9 +23,9 @@ resource "aws_s3_bucket" "this" {
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
   ignore_public_acls      = true
 }
 
